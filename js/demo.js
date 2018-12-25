e7b67ef37dce35cd4b80ede3e33a15a775c5037f
Steven <s_chen@hotmail.ca>
2018-12-24 23:50:03 -0800

    style changes

--- js/demo.js
+++ js/demo.js
@@ -1,8 +1,3 @@
-/**
- * Particleground demo
- * @author Jonathan Nicol - @mrjnicol
- */
-
 // This can be used to set the Particles Effects. Check README for more details!
 document.addEventListener('DOMContentLoaded', function () {
   particleground(document.getElementById('particles'), {
